5cefb58b3abc2b1b55787995aae69dfcfc7207ab
Minhyuk Park <minpark815@gmail.com>
2020-08-26 10:33:06 -0500

    Updates rf distance

--- rf_distance_two_trees.py
+++ rf_distance_two_trees.py
@@ -2,6 +2,14 @@ import sys
 
 import dendropy
 
+def preprocess_tree(tree):
+    tree.is_rooted = False
+    tree.resolve_polytomies(limit=2)
+    tree.collapse_basal_bifurcation()
+    tree.update_bipartitions(suppress_unifurcations=False)
+
+    return tree
+
 left_tree_path = sys.argv[1]
 right_tree_path = sys.argv[2]
 left_tree_test = dendropy.Tree.get(path=left_tree_path, schema="newick")
@@ -19,6 +27,9 @@ dataset.read(path=left_tree_path, schema="newick", taxon_namespace=namespace)
 dataset.read(path=right_tree_path, schema="newick", taxon_namespace=namespace)
 left_tree = dataset.tree_lists[0][0]
 right_tree = dataset.tree_lists[1][0]
+
+left_tree = preprocess_tree(left_tree)
+right_tree = preprocess_tree(right_tree)
 
 print(f"Num Taxa: {len(namespace)}")
 unweighted_rf_distance = dendropy.calculate.treecompare.unweighted_robinson_foulds_distance(left_tree, right_tree)
