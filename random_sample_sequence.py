8565b9685751f78810b5e26b9c0e762826a25832
Minhyuk Park <minpark815@gmail.com>
2020-12-07 19:40:09 -0600

    Updates random sample sequnece

--- random_sample_sequence.py
+++ random_sample_sequence.py
@@ -8,10 +8,12 @@ import click
 @click.option("--input-sequence", required=True, type=click.Path(exists=True), help="Input fasta sequence file")
 @click.option("--output-sequence", required=True, type=click.Path(), help="Ouput sequence file name")
 @click.option("--num-sequence", required=True, type=int, help="Number of sequences to randomly sample")
-def random_sample_sequence(input_sequence, output_sequence, num_sequence):
+@click.option("--seed", required=True, type=int, help="seed to use")
+def random_sample_sequence(input_sequence, output_sequence, num_sequence, seed):
     """This script induces the input tree on the sequence file
     """
     sequences = list(SeqIO.parse(open(input_sequence), "fasta"))
+    random.seed(seed)
     random.shuffle(sequences)
     SeqIO.write(sequences[:num_sequence], open(output_sequence, "w"), "fasta")
 
