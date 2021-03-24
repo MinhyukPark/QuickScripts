6d3d33aaf09f922d5d03acaf2b2539973e9e0d81
Minhyuk Park <minpark815@gmail.com>
2021-03-24 10:09:48 -0500

    Adds initial fragmentary decompose

--- decompose.py
+++ decompose.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 import click
@@ -11,9 +12,16 @@ import decomposer
 @click.command()
 @click.option("--input-tree", required=True, type=click.Path(exists=True), help="The input tree file in newick format")
 @click.option("--sequence-file", required=True, type=click.Path(exists=True), help="Aligned sequence file on the full taxa")
+@click.option("--fragmentary-sequence-file", required=False, type=click.Path(exists=True), help="Fragmentary aligned sequence file on some set of taxa")
 @click.option("--output-prefix", required=True, type=str, help="Output file prefix for each subset")
 @click.option("--maximum-size", required=True, type=int, help="Maximum size of output subsets")
-def decompose_tree(input_tree, sequence_file, output_prefix, maximum_size):
+@click.option("--longest-edge", is_flag=True, help="Specifying longest edge decomposition")
+@click.option("--full-length", required=False, type=int, help="Specifyng a length for the sequences")
+@click.option("--longest", required=False, is_flag=True, help="Samples from the longest sequences in the case of incomplete sampling")
+@click.option("--incomplete", required=False, type=int, help="Specifying the size of incomplete decomposition (incomplete < maximum-size)")
+@click.option("--support-threshold", required=False, type=float, help="Specifying a support threshold for decomposition", default=0.95)
+@click.option("--mode", type=click.Choice(["centroid", "support", "heuristic"], case_sensitive=False), required=False, default="centroid")
+def decompose_tree(input_tree, sequence_file, fragmentary_sequence_file, output_prefix, maximum_size, longest_edge, full_length, longest, incomplete, support_threshold, mode):
     '''This script decomposes the input tree and outputs induced alignments on the subsets.
     '''
     guide_tree = dendropy.Tree.get(path=input_tree, schema="newick")
@@ -23,7 +31,33 @@ def decompose_tree(input_tree, sequence_file, output_prefix, maximum_size):
     guide_tree.collapse_basal_bifurcation()
     guide_tree.update_bipartitions()
 
-    trees = decomposer.decomposeTree(guide_tree, maximum_size, mode="centroid")
+    # create mapping of closest sequencs
+    fragmentary_mapping = {}
+    for fragmentary_sequence in SeqIO.parse(open(fragmentary_sequence_file), "fasta"):
+        current_id = fragmentary_sequence.id
+        best_distance = len(fragmentary_sequence)
+        best_id = None
+        for full_length_sequence in SeqIO.parse(open(sequence_file), "fasta"):
+            current_hamming_distance = 0
+            for position,nucleotide in enumerate(fragmentary_sequence):
+                if(fragmentary_sequence[position] != "-" and full_length_sequence[position] != "-"):
+                    if(fragmentary_sequence[position] != full_length_sequence[position]):
+                        current_hamming_distance += 1
+            if(current_hamming_distance < best_distance):
+                best_distance = current_hamming_distance
+                best_id = full_length_sequence.id
+        if(best_id not in fragmentary_mapping):
+            fragmentary_mapping[best_id] = []
+        fragmentary_mapping[best_id].append(current_id)
+
+    trees = None
+    sys.stderr.write(str(fragmentary_mapping) + "\n")
+    if(fragmentary_sequence_file == None):
+        trees = decomposer.decomposeTree(guide_tree, maximum_size, support_threshold, mode=mode)
+    elif(fragmentary_sequence_file != None):
+        trees = decomposer.fragmentary_decompose_tree(guide_tree, maximum_size, fragmentary_mapping)
+
+
     clusters = []
     for tree in trees:
         keep = [n.taxon.label.replace("_"," ") for n in tree.leaf_nodes()]
@@ -37,9 +71,25 @@ def decompose_tree(input_tree, sequence_file, output_prefix, maximum_size):
         for cluster_index,cluster in enumerate(clusters):
             if(sequence.id.replace("_"," ") in cluster):
                 sequence_partitions[cluster_index].append(sequence)
+                fragmentary_sequences = fragmentary_mapping[sequence.id]
+                for fragmentary_sequence in fragmentary_sequences:
+                    sequence_partitions[cluster_index].append(fragmentary_sequence)
+
     for sequence_partition_index,sequence_partition in enumerate(sequence_partitions):
         SeqIO.write(sequence_partition, files[sequence_partition_index], "fasta")
 
+
+    incomplete_sequences = []
+    for sequence_partition_index,sequence_partition in enumerate(sequence_partitions):
+        if(incomplete != None):
+            if(longest):
+                current_list = sorted(sequence_partitions[sequence_partition_index], key=lambda x:len(x.seq.ungap("-")), reverse=True)
+                incomplete_sequences.extend(current_list[:incomplete])
+            else:
+                random.shuffle(sequence_partitions[sequence_partition_index])
+                incomplete_sequences.extend(sequence_partitions[sequence_partition_index][:incomplete])
+    if(incomplete != None):
+        SeqIO.write(incomplete_sequences, output_prefix + "incomplete.out", "fasta")
 
 
 if __name__ == "__main__":
--- decomposer.py
+++ decomposer.py
@@ -9,24 +9,99 @@ import numpy as np
 import dendropy
 from dendropy.utility import bitprocessing
 
+import copy
 import os
 import sys
 
-def decomposeTree(tree, maxSubsetSize, mode):
+def decomposeTree(tree, max_subset_size, support_threshold, mode):
+    num_leaves = len(tree.leaf_nodes())
+    return decompose_tree_helper(num_leaves, tree, max_subset_size, support_threshold, mode)
+
+def decompose_tree_helper(num_taxa, tree, max_subset_size, support_threshold, mode):
     numLeaves = len(tree.leaf_nodes())
-    if numLeaves > maxSubsetSize:
+    if mode == "heuristic" and numLeaves > max_subset_size * 1.5:
+        e = getBestHeuristicEdge(tree, max_subset_size, num_taxa)
+        t1, t2 = bipartitionByEdge(tree, e)
+        return decompose_tree_helper(num_taxa, t1, max_subset_size, support_threshold, mode) + decompose_tree_helper(num_taxa, t2, max_subset_size, support_threshold, mode)
+    elif mode != "heuristic" and numLeaves > max_subset_size:
         if mode == "centroid":
             e = getCentroidEdge(tree)
         elif mode == "random":
-            e = getCentroidEdgeRandom(tree, maxSubsetSize/3)
+            e = getCentroidEdgeRandom(tree, max_subset_size/3)
+        elif mode == "longest":
+            e = getLongestEdge(tree)
+        elif mode == "support":
+            e = getSupportEdge(tree, support_threshold)
 
         t1, t2 = bipartitionByEdge(tree, e)
-        return decomposeTree(t1, maxSubsetSize, mode) + decomposeTree(t2, maxSubsetSize, mode)
+        return decompose_tree_helper(num_taxa, t1, max_subset_size, support_threshold, mode) + decompose_tree_helper(num_taxa, t2, max_subset_size, support_threshold, mode)
     else:
         if numLeaves >= 1:
             return [tree]
         else:
             sys.exit("tree has fewer than 1 leaves!")
+
+def fragmentary_decompose_tree(tree, max_subset_size, fragmentary_mapping):
+    num_full_length_leaves = len(tree.leaf_nodes())
+    return fragmentary_decompose_tree_helper(num_full_length_leaves, tree, max_subset_size, fragmentary_mapping)
+
+def fragmentary_decompose_tree_helper(num_taxa, tree, max_subset_size, fragmentary_mapping):
+    num_full_length_leaves = len(tree.leaf_nodes())
+    num_fragmentary_leaves = get_num_fragmentary_leaves(tree.leaf_nodes(), fragmentary_mapping)
+    num_leaves = num_full_length_leaves + num_fragmentary_leaves
+    if(num_leaves > max_subset_size):
+        e = get_fragmentary_centroid_edge(tree, fragmentary_mapping)
+        t1, t2 = bipartitionByEdge(tree, e)
+        return fragmentary_decompose_tree_helper(num_taxa, t1, max_subset_size, fragmentary_mapping) + decompose_tree_helper(num_taxa, t2, max_subset_size, fragmentary_mapping)
+    else:
+        if num_leaves >= 1:
+            return [tree]
+        else:
+            sys.exit("tree has fewer than 1 leaves!")
+
+def get_num_fragmentary_leaves(leaf_nodes, fragmentary_mapping):
+    num = 0
+    for leaf_node in leaf_nodes:
+        if(leaf_node in fragmentary_mapping):
+            num += len(fragmentary_mapping[leaf_node])
+    return num
+
+
+def get_fragmentary_centroid_edge(tree, fragmentary_mapping):
+    num_full_length_leaves = len(tree.leaf_nodes())
+    num_fragmentary_leaves = get_num_fragmentary_leaves(tree.leaf_nodes(), fragmentary_mapping)
+    num_leaves = num_full_length_leaves + num_fragmentary_leaves
+    best_balance = float('inf')
+    best_edge = None
+    # sys.stderr.write("searching for best edge in num leaves:")
+    # sys.stderr.write(str(num_leaves) + str("\n"))
+    for edge in tree.postorder_edge_iter():
+        if edge.tail_node is None:
+            continue
+        # candidate_split_tree = copy.deepcopy(tree)
+        # t1, t2 = bipartitionByEdge(candidate_split_tree, edge)
+        # t1_full_length_leaves = len(t1.leaf_nodes())
+        # t1_fragmentary_leaves = get_num_fragmentary_leaves(t1.leaf_nodes(), fragmentary_mapping)
+        # t1_num_leaves = t1_full_length_leaves + t1_fragmentary_leaves
+        # t2_full_length_leaves = len(t2.leaf_nodes())
+        # t2_fragmentary_leaves = get_num_fragmentary_leaves(t2.leaf_nodes(), fragmentary_mapping)
+        # t2_num_leaves = t2_full_length_leaves + t2_fragmentary_leaves
+        # balance = max(t1_num_leaves, t2_num_leaves) - min(t1_num_leaves, t2_num_leaves)
+        t1_full_length_leaves = edge.bipartition.leafset_taxa(tree.taxon_namespace)
+        t1_fragmentary_leaves = get_num_fragmentary_leaves(t1_full_length_leaves, fragmentary_mapping)
+        t1_num_leaves = len(t1_full_length_leaves) + t1_fragmentary_leaves
+        t2_full_length_leaves = list(filter(lambda x: x not in t1_full_length_leaves, tree.leaf_nodes()))
+        t2_fragmentary_leaves = get_num_fragmentary_leaves(t2_full_length_leaves, fragmentary_mapping)
+        t2_num_leaves = len(t2_full_length_leaves) + t2_fragmentary_leaves
+        balance = max(t1_num_leaves, t2_num_leaves) - min(t1_num_leaves, t2_num_leaves)
+
+        if balance < best_balance:
+            best_balanace = balance
+            best_edge = edge
+    # sys.stderr.write(str(best_edge.head_node))
+    # sys.stderr.write(str(best_edge.length))
+    # sys.stderr.write(str(best_edge.head_node.label))
+    return bestEdge
 
 def bipartitionByEdge(tree, edge):
     newRoot = edge.head_node
@@ -40,13 +115,40 @@ def getCentroidEdge(tree):
     numLeaves = bitprocessing.num_set_bits(tree.seed_node.tree_leafset_bitmask)
     # numLeaves = len(tree.seed_node.leaf_nodes())
     bestBalance = float('inf')
+    sys.stderr.write("searching for best edge in num leaves:")
+    sys.stderr.write(str(numLeaves) + str("\n"))
     for edge in tree.postorder_edge_iter():
         if edge.tail_node is None:
             continue
         balance = abs(numLeaves/2 - bitprocessing.num_set_bits(edge.bipartition.leafset_bitmask))
+        sys.stderr.write("current_balance:")
+        sys.stderr.write(str(balance) + "\n")
         if balance < bestBalance:
             bestBalance = balance
             bestEdge = edge
+    sys.stderr.write(str(bestEdge.head_node))
+    sys.stderr.write(str(bestEdge.length))
+    sys.stderr.write(str(bestEdge.head_node.label))
+    return bestEdge
+
+def getSupportEdge(tree, support_threshold):
+    numLeaves = bitprocessing.num_set_bits(tree.seed_node.tree_leafset_bitmask)
+    # numLeaves = len(tree.seed_node.leaf_nodes())
+    bestBalance = float('inf')
+    sys.stderr.write("searching for best edge in num leaves:")
+    sys.stderr.write(str(numLeaves) + str("\n"))
+    for edge in tree.postorder_edge_iter():
+        if edge.tail_node is None:
+            continue
+        balance = abs(numLeaves/2 - bitprocessing.num_set_bits(edge.bipartition.leafset_bitmask))
+        sys.stderr.write("current_balance:")
+        sys.stderr.write(str(balance) + "\n")
+        if balance < bestBalance and edge.head_node.label is not None and float(edge.head_node.label) > support_threshold:
+            bestBalance = balance
+            bestEdge = edge
+    sys.stderr.write(str(bestEdge.head_node) + "\n")
+    sys.stderr.write(str(bestEdge.length) + "\n")
+    sys.stderr.write(str(bestEdge.head_node.label) + "\n")
     return bestEdge
 
 def getCentroidEdgeRandom(tree, minBound = 5):
@@ -65,3 +167,68 @@ def getCentroidEdgeRandom(tree, minBound = 5):
             candidates.append(edge)
     return np.random.choice(candidates)
 
+def getLongestEdge(tree):
+    numLeaves = bitprocessing.num_set_bits(tree.seed_node.tree_leafset_bitmask)
+    # numLeaves = len(tree.seed_node.leaf_nodes())
+    longeth_edge = None
+    longest_edge_length = 0
+    for edge in tree.postorder_edge_iter():
+        if edge.tail_node is None:
+            continue
+        current_length = edge.length
+        if longest_edge_length < current_length:
+            longest_edge_length = current_length
+            longest_edge = edge
+    return longest_edge
+
+
+def getBestHeuristicEdge(tree, max_subset_size, num_taxa):
+    num_leaves = bitprocessing.num_set_bits(tree.seed_node.tree_leafset_bitmask)
+    current_best_score = -1
+    current_best_edge = None
+    subset_L_size = 0
+    subset_r_size = 0
+    best_L_score = 0
+    best_R_score = 0
+    best_subset_L_size = 0
+    best_subset_R_size = 0
+    for edge in tree.postorder_edge_iter():
+        if edge.tail_node is None:
+            continue
+        if edge.head_node.label is not None:
+            subset_L_size = bitprocessing.num_set_bits(edge.bipartition.leafset_bitmask)
+            subset_R_size = num_leaves - bitprocessing.num_set_bits(edge.bipartition.leafset_bitmask)
+            # sys.stderr.write(str(subset_L_size) + ":" + str(subset_R_size) + "\n")
+            current_L_score = heuristic(float(edge.head_node.label), subset_L_size, max_subset_size, num_taxa)
+            current_R_score = heuristic(float(edge.head_node.label), subset_R_size, max_subset_size, num_taxa)
+            if current_best_score < sum([current_L_score, current_R_score]):
+                best_L_score = current_L_score
+                best_R_score = current_R_score
+                best_subset_L_size = subset_L_size
+                best_subset_R_size = subset_R_size
+                current_best_score = sum([current_L_score, current_R_score])
+                current_best_edge = edge
+
+    # sys.stderr.write(str(current_best_edge.head_node) + "\n")
+    # sys.stderr.write(str(current_best_edge.length) + "\n")
+    # sys.stderr.write(str(current_best_edge.head_node.label) + "\n")
+    sys.stderr.write(str(best_subset_L_size) + ":" + str(best_subset_R_size) + "\n")
+    sys.stderr.write(str(best_L_score) + ":" + str(best_R_score) + "\n")
+    return current_best_edge
+
+
+def heuristic(support_value, subset_size, max_subset_size, num_taxa):
+    support_component = None
+    size_component = None
+
+    support_component = (support_value)
+    size_component = 1 - (abs(subset_size - max_subset_size) / num_taxa)
+    raw_value = (0.6 * support_component) + (0.4 * size_component)
+
+    if support_value < 0.6:
+        raw_value = 0.01 * support_value
+    if subset_size < max_subset_size * 0.75:
+        raw_value = -1
+
+    return raw_value
+    # return max(0, min(0.9999, raw_value))
