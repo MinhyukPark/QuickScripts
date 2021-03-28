d2a774f23d68b59436195ea05a2006bbd2ad0ff6
Minhyuk Park <minpark815@gmail.com>
2021-03-27 23:00:55 -0500

    Adds parallel implementation

--- decompose.py
+++ decompose.py
@@ -1,14 +1,57 @@
+from collections import Counter
+from multiprocessing import current_process,Pool
 import random
+import heapq
 import sys
 
 import click
 import dendropy
-from scipy.spatial import distance
 from Bio import SeqIO
+import scipy
+from skbio import Sequence
+from skbio.sequence.distance import hamming
 
 import decomposer
 
+
 # argv guide_tree, subset_size, alignment_on_full_taxa, output_file_prefix
+
+POOL_SIZE=4
+
+def get_closest_sequence_mapping_helper(args):
+    return get_closest_sequence_mapping(*args)
+
+def get_closest_sequence_mapping(my_chunk, full_length_sequence_file):
+    partial_fragmentary_mapping = {}
+    heap_queue = []
+    stride_size = 300
+    sys.stderr.write("doing work on " + str(len(my_chunk)) + "\n")
+    full_length_dict = SeqIO.to_dict(SeqIO.parse(full_length_sequence_file, "fasta"))
+    for fragmentary_sequence in my_chunk:
+        alignment_sequence_length = len(fragmentary_sequence.seq)
+        fragmentary_id = fragmentary_sequence.id
+        best_distance = len(fragmentary_sequence)
+        best_id = None
+        for full_length_sequence_id,full_length_sequence in full_length_dict.items():
+            if(best_id is None):
+                current_score = scipy.spatial.distance.hamming(list(str(fragmentary_sequence.seq[:stride_size])), list(str(full_length_sequence.seq[:stride_size])))
+                to_digest = alignment_sequence_length
+                heapq.heappush(heap_queue, (current_score, to_digest, full_length_sequence_id))
+
+        while heapq:
+            previous_score,to_digest,current_sequence_id = heapq.heappop(heap_queue)
+            if(to_digest < 0):
+                best_id = current_sequence_id
+                break
+            else:
+                current_start_index = alignment_sequence_length - to_digest
+                current_score = scipy.spatial.distance.hamming(list(str(fragmentary_sequence.seq[current_start_index:current_start_index+stride_size])), list(str(full_length_dict[current_sequence_id].seq[current_start_index:current_start_index+stride_size])))
+                heapq.heappush(heap_queue, (previous_score + current_score, to_digest - stride_size, current_sequence_id))
+        if(best_id not in partial_fragmentary_mapping):
+            partial_fragmentary_mapping[best_id] = []
+        partial_fragmentary_mapping[best_id].append(fragmentary_id)
+    return partial_fragmentary_mapping
+
 
 @click.command()
 @click.option("--input-tree", required=True, type=click.Path(exists=True), help="The input tree file in newick format")
@@ -35,49 +78,41 @@ def decompose_tree(input_tree, sequence_file, fragmentary_sequence_file, output_
     # create mapping of closest sequencs
     fragmentary_mapping = {}
     if(fragmentary_sequence_file != None):
-        for fragmentary_sequence in SeqIO.parse(open(fragmentary_sequence_file), "fasta"):
-            current_id = fragmentary_sequence.id
-            best_distance = len(fragmentary_sequence)
-            best_id = None
-            for full_length_sequence in SeqIO.parse(open(sequence_file), "fasta"):
-                # current_hamming_distance = 0
-                current_hamming_distance = distance.hamming(list(str(fragmentary_sequence.seq)), list(str(full_length_sequence.seq)))
-                # for position,nucleotide in enumerate(fragmentary_sequence):
-                #     if(fragmentary_sequence[position] == "-" and full_length_sequence[position] != "-"):
-                #         current_hamming_distance += 1
-                #     elif(fragmentary_sequence[position] != "-" and full_length_sequence[position] == "-"):
-                #         current_hamming_distance += 1
-                #     elif(fragmentary_sequence[position] == "-" and full_length_sequence[position] == "-"):
-                #         current_hamming_distance += 0
-                #     elif(fragmentary_sequence[position] != "-" and full_length_sequence[position] != "-"):
-                #         if(fragmentary_sequence[position] != full_length_sequence[position]):
-                #             current_hamming_distance += 1
-                #         elif(fragmentary_sequence[position] == full_length_sequence[position]):
-                #             current_hamming_distance += 0
-
-                if(current_hamming_distance < best_distance):
-                    best_distance = current_hamming_distance
-                    best_id = full_length_sequence.id
-            if(best_id not in fragmentary_mapping):
+        worker_pool = Pool(processes=POOL_SIZE)
+        worker_args_arr = []
+        total_fragmentary_sequences = list(SeqIO.parse(open(fragmentary_sequence_file, "r"), "fasta"))
+        chunk_size = int(len(total_fragmentary_sequences) / POOL_SIZE)
+        for worker_id in range(POOL_SIZE):
+            current_chunk = None
+            if(chunk_size > 0):
+                if(worker_id == POOL_SIZE - 1):
+                    current_chunk = total_fragmentary_sequences[int(worker_id*chunk_size):]
+                else:
+                    current_chunk = total_fragmentary_sequences[int(worker_id*chunk_size):int((worker_id+1)*chunk_size)]
+            else:
+                current_chunk = total_fragmentary_sequences[int(worker_id):int(worker_id+1)]
+            sys.stderr.write("worker id: " + str(worker_id) + "is going to workng on length " + str(len(current_chunk)) + "\n")
+            worker_args_arr.append((current_chunk, sequence_file))
+        partial_fragmentary_mapping_arr = worker_pool.map(get_closest_sequence_mapping_helper, worker_args_arr)
+    sys.stderr.write(str(partial_fragmentary_mapping_arr))
+    for partial_fragmentary_mapping in partial_fragmentary_mapping_arr:
+        for best_id,fragment_id_arr in partial_fragmentary_mapping.items():
+            if (best_id not in fragmentary_mapping):
                 fragmentary_mapping[best_id] = []
-            fragmentary_mapping[best_id].append(current_id)
+            fragmentary_mapping[best_id].extend(fragment_id_arr)
+    sys.stderr.write(str(fragmentary_mapping))
 
     trees = None
     sys.stderr.write(str(fragmentary_mapping) + "\n")
     if(fragmentary_sequence_file == None):
-        sys.stderr.write("No fragmentary sequence file provided")
         trees = decomposer.decomposeTree(guide_tree, maximum_size, support_threshold, mode=mode)
     elif(fragmentary_sequence_file != None):
-        sys.stderr.write("Fragmentary sequence file provided")
-        sys.stderr.write("calling fragmentary_decompose_tree")
         trees = decomposer.fragmentary_decompose_tree(guide_tree, maximum_size, fragmentary_mapping)
     clusters = []
-    sys.stderr.write("the number of trees in trees (output of decompose) is " + str(len(trees)))
     for tree in trees:
         keep = [n.taxon.label.replace("_"," ") for n in tree.leaf_nodes()]
         clusters.append(set(keep))
     print(len(clusters))
-    sys.stderr.write("the number of trees in trees (output of decompose) should equal " + str(len(clusters)))
 
     files = [output_prefix + str(i) + ".out" for i in range(len(clusters))]
     sequence_partitions = [[] for _ in range(len(clusters))]
